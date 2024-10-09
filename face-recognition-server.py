e42bc3f7c6b3aa2653017545cb98d5fa6c2f583b
Talha Khalid <F2021266625@umt.edu.pk>
2024-10-09 23:46:31 +0500

    Memory Utilization is Improved

--- face-recognition-server.py
+++ face-recognition-server.py
@@ -4,22 +4,24 @@ import requests
 from flask import Flask, request, jsonify
 from deepface import DeepFace
 import numpy as np
+from io import BytesIO
 
 app = Flask(__name__)
 
 
 def get_image_from_url(image_url):
+    global response, img_data
     try:
-        response = requests.get(image_url)
-        img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+        response = requests.get(image_url, stream=True)
+        img_data = BytesIO(response.content)
+        img_array = np.asarray(bytearray(img_data.read()), dtype=np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
         if img is None:
             raise ValueError(f"Unable to load image from {image_url}")
         return img
-
-    except Exception as e:
-        print(f"Error downloading image from URL: {e}")
-        raise ValueError(f"Error downloading image: {str(e)}")
+    finally:
+        response.close()
+        img_data.close()
 
 
 @app.route('/extract-embedding', methods=['POST'])
@@ -31,10 +33,8 @@ def extract_embedding():
         img = get_image_from_url(image_url)
         embedding = DeepFace.represent(img_path=img, model_name="VGG-Face")[0]["embedding"]
         return jsonify({'embedding': embedding}), 200
-
-    except Exception as e:
-        print(f"Error extracting face embedding: {e}")
-        return jsonify({'error': str(e)}), 500
+    finally:
+        del img
 
 
 @app.route('/detect-face', methods=['POST'])
@@ -47,11 +47,9 @@ def detect_face():
         face_objs = DeepFace.extract_faces(img, detector_backend='opencv', enforce_detection=False)
         face_count = len(face_objs) if isinstance(face_objs, list) else 0
         return jsonify({'face_count': face_count}), 200
-
-    except Exception as e:
-        print(f"Error detecting face: {e}")
-        return jsonify({'error': str(e)}), 500
+    finally:
+        del img
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
